42344427d0fb2ff2f62ab8c502cd5a1881792450
Jason <66883665+json20@users.noreply.github.com>
2025-07-23 16:44:49 +0800

    add

--- lenet5.py
+++ lenet5.py
@@ -1,4 +1,4 @@
-import torch
+import torc
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
@@ -12,40 +12,33 @@ class LeNet5(nn.Module):
         super(LeNet5, self).__init__()
 
         # Input: (1, 32, 32)
-        self.conv1 = nn.Conv2d(
-            in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=0
-        )
-        # Output: (6, 28, 28) because 32 - 5 + 1 = 28
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        # Output: (6, 28, 28)
 
         self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
         # Output: (6, 14, 14)
 
-        self.conv2 = nn.Conv2d(
-            in_channels=6, out_channels=16, kernel_size=5, stride=1, padding=0
-        )
-        # Output: (16, 10, 10) because 14 - 5 + 1 = 10
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        # Output: (16, 10, 10)
 
         self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
         # Output: (16, 5, 5)
 
-        self.conv3 = nn.Conv2d(
-            in_channels=16, out_channels=120, kernel_size=5, stride=1, padding=0
-        )
-        # Output: (120, 1, 1) because 5 - 5 + 1 = 1
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, num_classes)
 
-        # Fully connected layers
-        self.fc1 = nn.Linear(120, 84)
-        self.fc2 = nn.Linear(84, num_classes)
+        self.tanh = nn.Tanh()
 
     def forward(self, x):
-        x = F.tanh(self.conv1(x))  # (6, 28, 28)
-        x = self.pool1(x)  # (6, 14, 14)
-        x = F.tanh(self.conv2(x))  # (16, 10, 10)
-        x = self.pool2(x)  # (16, 5, 5)
-        x = F.tanh(self.conv3(x))  # (120, 1, 1)
-        x = x.view(-1, 120)  # flatten
-        x = F.tanh(self.fc1(x))
-        x = self.fc2(x)
+        x = self.tanh(self.conv1(x))
+        x = self.pool1(x)
+        x = self.tanh(self.conv2(x))
+        x = self.pool2(x)
+        x = x.view(x.size(0), -1)  # flatten
+        x = self.tanh(self.fc1(x))
+        x = self.tanh(self.fc2(x))
+        x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
 
